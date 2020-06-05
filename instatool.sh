e85131ea449eed69a84ed8645d3a46f849bd301c
Mr Hacker <53078652+MrHackerOfficial@users.noreply.github.com>
2020-06-05 14:37:14 +0530

    Create instatool.sh

---
+++ instatool.sh
@@ -0,0 +1,152 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+echo
+echo
+echo
+echo $i"["$bi"•"$i"]"$me"──────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"      PLEASE SUBSCRIBE MR HACKER  "$i"             |"
+echo $i" |"$me"        INSTAGRAM HACKING TOOLS               "$i" |"
+echo $i" |"$me"=========================================="$i"|"
+echo $i" |"$pu" AUTHOR :"$ku"   KHOKAN DAS [MR HACKER]"$i"        |"
+echo $i" |"$pu" FACEBOOK :"$ku"  KHOKAN DAS"$i"                   |"
+echo $i" |"$pu" INSTAGRAM  :"$cy"Impish_khoka"$i"                 |"
+echo $i" |"$pu" Contact  WhatsApp :"$ku" 918906791455"$i"         |"
+echo $i" |"$pu" Contact Gmail :"$ku"    khoka.kd@gmail.com"$i"    |"
+echo $i"["$bi"•"$i"]"$me"──────────────────────────────────"$i"["$bi"•"$i"]"
+echo
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me" 1"$i" |"$cy" BRUTEFORCE INSTA         "$i"|"$i"    |"$cy"  "$i" |"
+echo $i"|"$me" 2"$i" |"$cy" BRUTEFORCE INSTA 2       "$i"|"$i"    |"$cy" M"$i" |"
+echo $i"|"$me" 3"$i" |"$cy" BRUTEFORCE INSTA BADGE   "$i"|"$i"    |"$cy" R"$i" |"
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me" 4"$i" |"$cy"  CLONING INSTAGRAM ID    "$i"|"$i"    ─────"
+echo $i"|"$me" 5"$i" |"$cy" PROFILE GUARD FACEBOOK   "$i"|"$i"    |"$cy" H "$i" |"
+echo $i"|"$me" 6"$i" |"$cy" REPORT FACEBOOK WORK     "$i"|"$i"    |"$cy" A"$i"  |"
+echo $i"|"$me" 7"$i" |"$cy" PHISING INSTAGRAM 1      "$i"|"$i"    |"$cy" C"$i"  |"
+echo $i"|"$me" 8"$i" |"$cy" PHISING INSTAGRAM 2      "$i"|"$i"    |"$cy" K "$i" |"
+echo $i"|"$me" 9"$i" |"$ch" INSTALL ALL REQUIREMENT  "$i"|"$i"    |"$cy" R "$i" |"
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me" X"$i" |"$cy"     EXIT    PROGRAM      "$i"|"$i"    |"$cy"   "$i" |"
+echo $i"────────────────────────────────    ─────"
+echo
+echo $me"┌==="$bi"["$i"MR HACKER"$bi"]"$me"======"$bi"["$i""Support me""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Pure-L0G1C/Instagram.git
+cd Instagram
+chmod +x *
+pip3 install -r requirements.txt
+python3 instagram.py -h
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Stephin-Franklin/Hack-Insta.git
+cd Hack-Insta
+chmod +x *
+python2 hackinsta.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "Under maintains, Ctl+c =close"|lolcat
+sleep 1
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "Maintains" |lolcat
+sleep 1
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "Maintains"|lolcat
+sleep 1
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "F A I L"|lolcat
+sleep 1
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+chmod +x *
+bash shellphish.sh
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U C C E S "|lolcat
+fi
+
+if [ $pil = X ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"THANKS FOR USEING"
+sleep 2
+echo $i"join Telegram :- MrHacker for New script "
+sleep 2
+echo $ku"WhatsApp :"$i" 918906791455"
+echo $ku"Instagram :"$i" Impish_khoka"
+sleep 2
+echo $pur">> Thanks  &  Support me <<"
+exit
+fi
