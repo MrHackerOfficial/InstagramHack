68dc8fc960b917b7f26030def6d64b163f703afc
Mr Hacker <53078652+MrHackerOfficial@users.noreply.github.com>
2020-07-14 14:55:35 +0530

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,15 @@
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U C C E S "|lolcat
